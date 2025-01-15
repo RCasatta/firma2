c9dd70448dd6d49db71a254edb15c2550ced81c8
Riccardo Casatta <riccardo@casatta.it>
2025-01-15 13:21:38 +0100

    use unstable for raspi image

--- build-raspi4-image/flake.nix
+++ build-raspi4-image/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Build Raspberry PI 4 image";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     multiqr_input = {
       url = "github:RCasatta/multiqr";
       inputs.nixpkgs.follows = "nixpkgs";
